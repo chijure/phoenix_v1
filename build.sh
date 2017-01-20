ed4ceda647fabd1461ea285c443fc16c08f5e858
Chijure <chijure100x100to@gmail.com>
2017-01-20 17:46:27 -0500

    Add: build.sh and anyKernel Signed-off-by: Jhuan Reategui <chijure100x100to@gmail.com>

---
+++ build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+kernel="Phoenix"
+variant="v1"
+toolchain=~/arm-eabi-4.7/bin
+toolchain2="arm-eabi-"
+config="v1-perf_rev_10_defconfig"
+kerneltype="zImage"
+jobcount="-j$(grep -c ^processor /proc/cpuinfo)"
+
+	export ARCH=arm
+	export CROSS_COMPILE=$toolchain/"$toolchain2"
+
+	read -p "Previous build found, clean working directory..(y/n)? : " cchoice
+	case "$cchoice" in
+		y|Y )
+			rm -rf arch/arm/boot/"$kerneltype"
+			make clean && make mrproper && rm -rf zip-creator/*.zip zip-creator/zImage zip-creator/system/lib/modules/*.ko
+			echo "Working directory cleaned...";;
+		 n|N ) echo "continuing...";;
+		* ) echo "invalid option"; sleep 2 ; build.sh;;
+	esac
+
+echo "now building the kernel"
+
+START=$(date +%s)
+
+	make "$config"
+	make "$jobcount"
+
+# the zip creation
+if [ -f arch/arm/boot/"$kerneltype" ]; then
+
+    rm -f zip-creator/kernel/$kerneltype
+
+   cp arch/arm/boot/$kerneltype zip-creator/$kerneltype
+	# changed antdking "now copy all created modules"
+	# modules
+	# (if you get issues with copying wireless drivers then it's your own fault for not cleaning)
+
+	find . -name *.ko | xargs cp -a --target-directory=zip-creator/system/lib/modules/
+
+	zipfile="$kernel-$variant.zip"
+	cd zip-creator
+	rm -f *.zip
+	zip -r $zipfile * -x *kernel/.gitignore*
+
+	echo "zip saved to zip-creator/$zipfile"
+	
+else # [ -f arch/arm/boot/"$kerneltype" ]
+    echo "the build failed so a zip won't be created"
+fi # [ -f arch/arm/boot/"$kerneltype" ]
+
+END=$(date +%s)
+BUILDTIME=$((END - START))
+B_MIN=$((BUILDTIME / 60))
+B_SEC=$((BUILDTIME - E_MIN * 60))
+echo -ne "\033[32mBuildtime: "
+[ $B_MIN != 0 ] && echo -ne "$B_MIN min(s) "
+echo -e "$B_SEC sec(s)\033[0m"
